82d637236dd5450c2ae5fb1412866d7d5e8595ff
kauesanto <129408833+kauesanto@users.noreply.github.com>
2023-11-27 10:02:09 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
 document.getElementById("demo").style.display = "block";
+<button onclick="exibirAlerta()">Clique para exibir o alerta</button>
+`function verificarFormulario() {`
+
+`if (document.getElementById("nome").value === "" || document.getElementById("email").value === "") {`
+
+`alert("Por favor, preencha todos os campos obrigatórios.");`
+
+`} else {`
+
+`alert("Formulário enviado com sucesso!")
